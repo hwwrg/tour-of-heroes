c79186870c5cf8e377bb75ec58b71cdcb7364c1f
hwwrg <86141485+hwwrg@users.noreply.github.com>
2022-04-11 15:18:21 +0200

    Add hero-search

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -12,7 +12,7 @@ import { MessageService } from './message.service'
 })
 
 export class HeroService {
-  deleteHero(id: number):Observable<Hero> {
+  deleteHero(id: number): Observable<Hero> {
     const url = `${this.heroesUrl}/${id}`;
 
     return this.http.delete<Hero>(url, this.httpOptions).pipe(
@@ -22,7 +22,7 @@ export class HeroService {
   }
 
   /** POST: add a new hero to the server */
-  addHero(hero: Hero):Observable<Hero> {
+  addHero(hero: Hero): Observable<Hero> {
     return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
       tap((newHero: Hero) => this.log(`added hero w/ id=${newHero.id}`)),
       catchError(this.handleError<Hero>('addHero'))
@@ -30,8 +30,8 @@ export class HeroService {
   }
 
   httpOptions = {
-    headers: new HttpHeaders({'Content-Type': 'application/json'})
-   }
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  }
 
   /** PUT: update the hero on the server */
   updateHero(hero: Hero): Observable<any> {
@@ -71,6 +71,23 @@ export class HeroService {
     return this.http.get<Hero>(url).pipe(
       tap(_ => this.log(`fetched hero id=${id}`)),
       catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
+  }
+
+  /* GET heroes whose name contains search term */
+  searchHeroes(term: string): Observable<Hero[]> {
+    if (!term.trim()) {
+      // if not search term, return empty hero array.
+      return of([]);
+    }
+    
+    return this.http.get<Hero[]>(`${this.heroesUrl}/?name=${term}`).pipe(
+      tap(
+        x => x.length ?
+          this.log('found heroes matching "${term}"') :
+          this.log('no heroes matching "${term}"')
+      ),
+      catchError(this.handleError<Hero[]>('searchHeroes', []))
     );
   }
 
